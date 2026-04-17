84b4c5f5b68915b65ee789a87dac50533baa76c2
leelah-jaffer <jafferleelah6@gmail.com>
2026-04-17 11:44:19 +0200

    decorated UI

--- app/src/main/java/com/example/songapp/MainActivity.kt
+++ app/src/main/java/com/example/songapp/MainActivity.kt
@@ -1,19 +1,24 @@
 package com.example.songapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.songapp.Song //only if you have issues if the package have completely different names
 
-
+//the main activity is where we do everything, taking the blueprints and executing the actions that is happening in the classes
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        var songShow = findViewById<TextView>(R.id.songDisplayId)
 
         //Accessing song class with specific attributes
         var song1 = Song("Snooze", "Sza", 2022,
