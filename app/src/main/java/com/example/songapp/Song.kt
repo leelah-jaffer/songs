1f991c7d420a363c2478eaabc3cfa54f03b9b43c
leelah-jaffer <jafferleelah6@gmail.com>
2026-04-17 10:30:24 +0200

    added song functioning into song class

--- app/src/main/java/com/example/songapp/Song.kt
+++ app/src/main/java/com/example/songapp/Song.kt
@@ -1,4 +1,41 @@
 package com.example.songapp
 
+import android.util.Log
+
 class Song {
+    // the properties
+    var name: String = ""
+    var artists: String = ""
+    var year: Int = 0
+    var duration: String = ""
+
+    //function to play song
+    fun play(){
+        Log.v("Song", "$name is playing")
+
+    }
+
+    //function to pause song
+    fun Pause(){
+        Log.v("Song", "$name is paused")
+
+
+    }
+
+    //function to stop song
+    fun Stop(){
+        Log.v("Song", "$name is stopped")
+
+
+    }
+
+    //function to favourite song
+    fun favourite() : Boolean{
+        Log.v("Song", "$name is favourite")
+        //Boolean made true
+        return true
+
+
+    }
+
 }
